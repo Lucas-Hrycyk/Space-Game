f45b466434de41443a714bab1c57d460b1d2f0d3
Lucas Antonio Alves Hrycyk <lucasantonio_02@hotmail.com>
2024-12-08 23:19:56 -0300

    Melhorias e Correções

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from random import randint 
+from random import Random
 import sys
 
 # Inicializa o Pygame
@@ -8,12 +10,17 @@ pygame.display.set_caption("Meu Jogo 2D")
 
 LARGURA, ALTURA = 1080, 720
 tela = pygame.display.set_mode((LARGURA, ALTURA))
-background_Game = pygame.image.load('Jogo/Imagens/Background.jpg')
-player = pygame.image.load('Jogo/Imagens/Player.png')
+background_Game = pygame.image.load('Space-Game/Imagens/Background.jpg')
+player = pygame.image.load('Space-Game/Imagens/Player.png')
+inimigo = pygame.image.load('Space-Game/Imagens/Inimigo.png')
 
 posicao_X = 480
 posiicao_Y = 520
 velocidade = 1
+
+pos_inimigo_x = 430
+pos_inimigo_y = 50
+velocidade_inimigo = 5
 
 while True:
     for evento in pygame.event.get():
@@ -40,6 +47,10 @@ while True:
 
     # Nave do Player
     tela.blit(player, (posicao_X, posiicao_Y))
+
+    tela.blit(inimigo, (580,40))
+    tela.blit(inimigo, (480,100))
+    tela.blit(inimigo, (380,40))
 
     # Atualizar a tela
     pygame.display.update()
