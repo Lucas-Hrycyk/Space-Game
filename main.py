0777bbd8c6815320359ddd2affb019d2357d4c6b
Lucas Antonio Alves Hrycyk <lucasantonio_02@hotmail.com>
2024-12-09 06:54:08 -0300

    Movimento nave inimiga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
-from random import randint 
-from random import Random
+from random import randint, choice
 import sys
 
 # Inicializa o Pygame
@@ -15,12 +14,16 @@ player = pygame.image.load('Space-Game/Imagens/Player.png')
 inimigo = pygame.image.load('Space-Game/Imagens/Inimigo.png')
 
 posicao_X = 480
-posiicao_Y = 520
-velocidade = 1
+posicao_Y = 520
+velocidade = 5
 
-pos_inimigo_x = 430
-pos_inimigo_y = 50
-velocidade_inimigo = 5
+pos_inimigo_x = randint(0, LARGURA - inimigo.get_width())
+pos_inimigo_y = randint(0, ALTURA // 2)
+velocidade_inimigo_x = randint(-5, 5)
+velocidade_inimigo_y = randint(-5, 5)
+
+relogio = pygame.time.Clock()
+mudanca_tempo = 0
 
 while True:
     for evento in pygame.event.get():
@@ -30,28 +33,41 @@ while True:
 
     comando = pygame.key.get_pressed()
 
-    if comando[pygame.K_UP or pygame.K_w]:
-        posiicao_Y -= velocidade
-
-    if comando[pygame.K_LEFT or pygame.K_a]:
+    if comando[pygame.K_UP] or comando[pygame.K_w]:
+        posicao_Y -= velocidade
+    if comando[pygame.K_LEFT] or comando[pygame.K_a]:
         posicao_X -= velocidade
-
-    if comando[pygame.K_DOWN or pygame.K_s]:
-        posiicao_Y += velocidade
-
-    if comando[pygame.K_RIGHT or pygame.K_d or pygame.K_d]:
+    if comando[pygame.K_DOWN] or comando[pygame.K_s]:
+        posicao_Y += velocidade
+    if comando[pygame.K_RIGHT] or comando[pygame.K_d]:
         posicao_X += velocidade
 
+    # Atualizar posição do inimigo
+    pos_inimigo_x += velocidade_inimigo_x
+    pos_inimigo_y += velocidade_inimigo_y
+
+    # Verificar colisão com bordas da tela
+    if pos_inimigo_x <= 0 or pos_inimigo_x >= LARGURA - inimigo.get_width():
+        velocidade_inimigo_x *= -1
+    if pos_inimigo_y <= 0 or pos_inimigo_y >= ALTURA // 2 - inimigo.get_height():
+        velocidade_inimigo_y *= -1
+
+    # Alterar direção aleatoriamente em intervalos de tempo
+    mudanca_tempo += 1
+    if mudanca_tempo > 30:  # Muda de direção a cada 30 quadros (~0.5 segundos)
+        velocidade_inimigo_x = randint(-5, 5)
+        velocidade_inimigo_y = randint(-5, 5)
+        mudanca_tempo = 0
+
     # Preencher a tela com a Imagem de fundo
-    tela.blit(background_Game, (0,0))
+    tela.blit(background_Game, (0, 0))
 
     # Nave do Player
-    tela.blit(player, (posicao_X, posiicao_Y))
+    tela.blit(player, (posicao_X, posicao_Y))
 
-    tela.blit(inimigo, (580,40))
-    tela.blit(inimigo, (480,100))
-    tela.blit(inimigo, (380,40))
+    # Nave Inimiga
+    tela.blit(inimigo, (pos_inimigo_x, pos_inimigo_y))
 
     # Atualizar a tela
     pygame.display.update()
-
+    relogio.tick(60)
